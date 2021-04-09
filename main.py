744219c6beb1e29d903ec023729e26fa722065ee
Brandon Williams <brando5393@gmail.com>
2021-04-09 01:24:52 +0000

    basic screen back up and running

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import downloader
 from screen import Screen
 from kivy.app import App
 
-
 class Program(App):
-  def build(self):
-    # this function returns the "top-level" component of the app all other components must be children
-    self.title="YouTube Video Downloader"
-    return Screen
+ def build(self):
+   self.title="YouTube Video Downloader"
+   return Screen()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   Program().run()
--- screen.py
+++ screen.py
@@ -8,7 +8,6 @@ class Screen(GridLayout):
     # set columns
     self.cols = 1
     # add widgets
-    self.url_input = TextInput(multiline=False)
     self.add_widget(Label(text="YouTube Video Downloader"))
     self.add_widget(Label(text="Paste your video link below"))
-    self.video_url = TextInput(multiline=False)
+    self.add_widget(TextInput(multiline=False))
